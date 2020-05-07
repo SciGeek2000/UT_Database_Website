63c2aa6e412e56f0eb17899d9aa5eeb528837d41
Thomas Ersevim <42191922+SciGeek2000@users.noreply.github.com>
2020-05-07 08:26:28 -0400

    Update Google_Sheets_Scraper.py

--- Google_Sheets_Scraper.py
+++ Google_Sheets_Scraper.py
@@ -1,7 +1,14 @@
+#This program requires a credentials-sheets.json file to function properly. Obtain one on the google API website. 
 import ezsheets
 import pandas as pd
+
+#Gets spreadsheet from page id (found in url)
 ss = ezsheets.Spreadsheet('1KjXGhOFzi0SZPsozpKzxGjVtfr4kkS_Hv5EigUwKOj8')
+
+#Downloads data first as a Excel sheet (only way to preserve all of the sheets)
 ss.downloadAsExcel('Google_Sheets_to_Excel.xlsx');
+
+#Converts each relavent sheet of the Excel workbook into a .csv file
 pd.read_excel('Google_Sheets_to_Excel.xlsx', sheet_name=ss[1].title).to_csv('CSV_Backplane.csv', index=False)
 pd.read_excel('Google_Sheets_to_Excel.xlsx', sheet_name=ss[2].title).to_csv('CSV_DCB.csv', index=False)
 pd.read_excel('Google_Sheets_to_Excel.xlsx', sheet_name=ss[3].title).to_csv('CSV_LVR.csv', index=False)
